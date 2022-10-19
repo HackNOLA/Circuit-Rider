4d51fe9473f178239c8b57a99ad31b0f1f695260
Akintunde Salako Pounds <akinpds@gmail.com>
2022-10-19 09:01:23 -0500

    Changed About

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -7,11 +7,10 @@ const About = () => {
                 <div className="col s12 center-align">
                     <img alt="logo" src={require("../../images/text-logo.png")} style={{ marginTop: 50 + "px", marginBottom: 50 + "px", height: 50, width: 350}} />
                     <p className="event-text" style={{ fontFamily: "Arcade" }}>
-                        Welcome to Circuit Rider, an oregon-trail style survival game in which you try to
-                        navigate your way through a post-apocalyptic wasteland and make your way to safety.
-                        Along the way, you’ll face choices that will affect your health, fuel levels, food,
-                        or money. Be careful, if your health, fuel, or food hit zero, it will be game over.
-                        Can you survive Circuit Rider?</p>
+                    Circuit Rider is an interactive, web game that teaches players about Methodism. 
+                    It is based on the retro game Oregon Trail, which allows players to go on different 
+                    adventures based on decisions players make with their character. Can you survive 
+                    Circuit Rider?</p>
                 </div>
             </div>
         </div>
