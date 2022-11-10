0f9b80d296a305aabdf05285e4bbcb98c8eb7ad6
Akintunde Salako Pounds <akinpds@gmail.com>
2022-11-10 14:04:04 -0600

    Gamestory part1 (#16)
    * updated gamestory up until holy club meeting
    
    * editted wallpaper
    
    * Added importance to playercol bg
    
    * Changed About
    
    * added music player
    
    * added coirety meeting text
    
    * updated credits

--- client/src/data/Characters.js
+++ client/src/data/Characters.js
@@ -1,69 +1,69 @@
 const playableCharacters = [
   {
     player_id: 0,
-    name: "Adam",
-    occupation: "London Worker",
+    name: 'Adam',
+    occupation: 'Colonial Farmer',
     image:
-      "https://i0.wp.com/southcountynews.org/wp-content/uploads/2014/08/circut-rider.jpg?ssl=1",
+      'https://i0.wp.com/southcountynews.org/wp-content/uploads/2014/08/circut-rider.jpg?ssl=1',
     hp: 120,
     money: 80,
     horse: 1,
     food: 100,
     state: 0,
-    inventory: []
+    inventory: [],
   },
   {
     player_id: 1,
-    name: "Caitlan",
-    occupation: "Oxford Student",
+    name: 'Caitlan',
+    occupation: 'Frontier Explorer',
     image:
-      "https://www.northwindprints.com/p/473/traveling-minister-american-frontier-5885514.jpg.webp",
+      'https://www.northwindprints.com/p/473/traveling-minister-american-frontier-5885514.jpg.webp',
     hp: 90,
     money: 110,
     horse: 1,
     food: 200,
     state: 0,
-    inventory: []
+    inventory: [],
   },
   {
     player_id: 2,
-    name: "Kevin",
-    occupation: "Circuit Rider",
+    name: 'Kevin',
+    occupation: 'Circuit Rider',
     image:
-      "http://4.bp.blogspot.com/-MTJu43wrdoE/Tj7x4XLixUI/AAAAAAAABhI/6Ra6HQwPhLk/s1600/preacher.jpg",
+      'http://4.bp.blogspot.com/-MTJu43wrdoE/Tj7x4XLixUI/AAAAAAAABhI/6Ra6HQwPhLk/s1600/preacher.jpg',
     hp: 100,
     money: 20,
     horse: 1,
     food: 80,
     state: 0,
-    inventory: []
+    inventory: [],
   },
   {
     player_id: 3,
-    name: "Liam",
-    occupation: "???",
+    name: 'Liam',
+    occupation: '???',
     image:
-      "https://kanka-user-assets.s3.eu-central-1.amazonaws.com/notes/5cb8ac036c7ac_636207488454374266-1324134898_silhouette-man.jpg",
-    hp: "???",
-    money: "???",
-    horse: "???",
-    food: "???",
+      'https://kanka-user-assets.s3.eu-central-1.amazonaws.com/notes/5cb8ac036c7ac_636207488454374266-1324134898_silhouette-man.jpg',
+    hp: '???',
+    money: '???',
+    horse: '???',
+    food: '???',
     state: 0,
-    inventory: []
+    inventory: [],
   },
   {
     player_id: 4,
-    name: "Loren",
-    occupation: "???",
+    name: 'Loren',
+    occupation: '???',
     image:
-      "https://kanka-user-assets.s3.eu-central-1.amazonaws.com/notes/5cb8ac036c7ac_636207488454374266-1324134898_silhouette-man.jpg",
-    hp: "???",
-    money: "???",
-    horse: "???",
-    food: "???",
+      'https://kanka-user-assets.s3.eu-central-1.amazonaws.com/notes/5cb8ac036c7ac_636207488454374266-1324134898_silhouette-man.jpg',
+    hp: '???',
+    money: '???',
+    horse: '???',
+    food: '???',
     state: 0,
-    inventory: []
-  }
-];
+    inventory: [],
+  },
+]
 
-export default playableCharacters;
+export default playableCharacters
--- client/src/pages/Credits/index.js
+++ client/src/pages/Credits/index.js
@@ -1,31 +1,47 @@
-import React from 'react';
+import React from 'react'
 
 const Credits = () => {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col s12 center-align">
-                    <div className="row" style={{ marginTop: 50 + "px" }}>
-                        <h4>CREDITS</h4>
-                    </div>
-                    <div className="row">
-                        <h4 className="credit-title">Product Owner</h4>
-                        <p className="event-text" style={{ fontFamily: "Arcade" }}>Matt Rawle</p>
-                    </div>
-                    <div className="row">
-                        <h4 className="credit-title">Full Stack Developer</h4>
-                        <p className="event-text" style={{ fontFamily: "Arcade" }}>Eric O'Neal Jr</p>
-                    </div>
-                    <div className="row">
-                        <h4 className="credit-title">Full Stack Developer</h4>
-                        <p className="event-text" style={{ fontFamily: "Arcade" }}>Akin Pounds</p>
-                    </div>
-                </div>
-
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col s12 center-align">
+          <div className="row" style={{ marginTop: 50 + 'px' }}>
+            <h4>CREDITS</h4>
+          </div>
+          <div className="row">
+            <h4 className="credit-title">Product Owner</h4>
+            <p className="event-text" style={{ fontFamily: 'Arcade' }}>
+              Matt Rawle
+            </p>
+          </div>
+          <div className="row">
+            <h4 className="credit-title">Full Stack Developer</h4>
+            <p className="event-text" style={{ fontFamily: 'Arcade' }}>
+              Eric O'Neal Jr
+            </p>
+          </div>
+          <div className="row">
+            <h4 className="credit-title">Full Stack Developer</h4>
+            <p className="event-text" style={{ fontFamily: 'Arcade' }}>
+              Akin Pounds
+            </p>
+          </div>
+          <div className="row">
+            <h4 className="credit-title">Music by:</h4>
+            <p className="event-text" style={{ fontFamily: 'Arcade' }}>
+              Mr. Tyler Larson
+            </p>
+          </div>
+          <div className="row">
+            <h4 className="credit-title">Art by:</h4>
+            <p className="event-text" style={{ fontFamily: 'Arcade' }}>
+              Hannah Wilson
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  )
 }
 
-
-export default Credits;
+export default Credits
